faab1cd20dd9c5612c1cf404d3a68054278bc38f
talha <muhammedtalhasaglam0@gmail.com>
2021-02-11 14:41:01 +0300

    Added new paths.

--- Chess-Game-main/Slippy Chess/Assets/Scripts/EnemyController.cs
+++ Chess-Game-main/Slippy Chess/Assets/Scripts/EnemyController.cs
@@ -6,17 +6,6 @@ public class EnemyController : MonoBehaviour
 {
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnCollisionEnter(Collision collision)
     {
